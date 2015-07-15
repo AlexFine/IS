d52a490692d0df25f098f77fb22caadfccbb3356
AlexFine <alexkfine2@gmail.com>
2015-07-14 23:53:38 -0700

    Finding Prime Numbers
    Trying to write an algorithm that finds all the prime numbers between 0
    and n. Running into a lot of syntax problems. Well figure out as time
    comes.

--- ctut.cpp
+++ ctut.cpp
@@ -3,24 +3,29 @@
 #include <string>
 #include <vector>
 #include <fstream>
-using namespace std; 
-
+using namespace std;
+#include <math.h>
 
 int main()
 {
-vector <float> primenums = {1, 2, 3};
+    vector <float> primenums = {2, 3, 5};
 
-for (float i = 0; i < 10000; i++){
-//cout << primenums[i] << endl;
-for (float j = 0; j < 100; j++){
-if(! i % primenums[j] == 0){
-	cout << i << endl;
-	primenums.insert(primenums.begin(), i);
+    for (float i = 1; i < 10000; i++){
+        //cout << primenums[i] << endl;
+        for (float j = 1; j < 100; j++){
+            if(fmod(i,primenums[j]) == 0){
+            }else{
+                cout << i << endl;
+        primenums.insert(primenums.begin(), i);
+            }
+        //primenums.insert(primenums.begin(), i);
+        //cout << primenums[j] << endl;
+        //cout << primenums.size() << endl;
+        }
+        //cout << primenums[i] << endl;
+    }
+    for (int i = 0; i < 100; i++){
+        cout << primenums[i] << endl;
+    }
+    return 0;
 }
-}
-}
-
-return 0;
-
-}
-
