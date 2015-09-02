9292116903fcf3d54cc14ce313efe3d6f0906196
AlexFine <alexkfine2@gmail.com>
2015-09-02 08:17:48 -0700

    Successful Insertion Sort Algorithm

--- Quick Code/InsertionSort.cpp
+++ Quick Code/InsertionSort.cpp
@@ -5,22 +5,28 @@
 #include <fstream>
 using namespace std;
 #include <math.h>
-int foo [5] = { 14, 4, 144, 20, 13411 };
+int foo [5] = { 2, 24, 242, 20, 3 };
 
 int main ()
 {
-    for(int j = 0; j < (sizeof(foo)/sizeof(*foo)); j++){
-
-        while(foo[j-1] > foo[j]){
-            //swap(foo[j-1], foo[j]);
-            int temp = foo[j];
-            //cout << temp << endl;
-            foo[j] = foo[j-1];
-            //cout << foo[j] << endl;
-            foo[j-1] = temp;
-            //cout << foo[j-1] << endl;
+    for (int k= 0; k < (sizeof(foo)/sizeof(*foo)); k++){
+        for(int j = 0; j < (sizeof(foo)/sizeof(*foo)); j++){
+            for (int i = 0; i < j+1; i++){
+                while(foo[j-1] > foo[j]){
+                    //swap(foo[j-1], foo[j]);
+                    int temp = foo[j];
+                    //cout << temp << endl;
+                    foo[j] = foo[j-1];
+                    //cout << foo[j] << endl;
+                    foo[j-1] = temp;
+                    //cout << foo[j-1] << endl;
+                }
+                //Should work for an insertion sort. Getting a strange error where it repeats elements. weird.
+            }
         }
-        //Should work for an insertion sort. Getting a strange error where it repeats elements. weird.
+    }
+        //cout << "Sorted array is: " << endl;
+    for(int j = 0; j < (sizeof(foo)/sizeof(*foo)); j++){
         cout << foo[j] << endl;
     }
     cout << "test" << endl;
