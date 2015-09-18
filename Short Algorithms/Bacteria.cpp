fa5e2b0b532b33b24ebbce65257764e1af1dbe44
AlexFine <alexkfine2@gmail.com>
2015-09-17 21:11:26 -0700

    Worked on some code forces problems today

---
+++ Short Algorithms/Bacteria.cpp
@@ -0,0 +1,23 @@
+
+// Example items to include
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <math.h>
+using namespace std;
+// main() is where program execution begins.
+int days;
+int back;
+int more;
+int main()
+{
+    int days = 10*10*10*10;
+    for (int i=1; i<days+1; i++){
+        cin >> more;
+        back = back + back + more;
+    }
+    cout << 
+
+    return 0;
+}
